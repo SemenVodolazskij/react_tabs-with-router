4af9562494fe54b3b43b173275e2b5852b953893
SemenVodolazskij <vodolazskijsemen@gmail.com>
2024-12-27 12:29:39 +0200

    develop

---
+++ src/components/TabsPage.tsx
@@ -0,0 +1,33 @@
+import { Link, Outlet, useParams } from 'react-router-dom';
+import { tabs } from '../App';
+import classNames from 'classnames';
+
+export const TabsPage: React.FC = () => {
+  const { tabId } = useParams();
+  const selectedTab = tabs.find(tab => tab.id === tabId);
+
+  return (
+    <>
+      <div className="container">
+        <h1 className="title">Tabs page</h1>
+        <div className="tabs is-boxed">
+          <ul>
+            {tabs.map(tab => (
+              <li
+                key={tab.id}
+                data-cy="Tab"
+                className={classNames({ 'is-active': tab.id === tabId })}
+              >
+                <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="block" data-cy="TabContent">
+          {selectedTab ? selectedTab.content : 'Please select a tab'}
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
+};
